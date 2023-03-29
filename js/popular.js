cc5a9cf98861928f09b83d781aa4214f2127653c
taofi <taofi.stop.lego@gmail.com>
2023-03-29 22:40:47 +0300

    dots added in popular slider +2 pages

--- js/popular.js
+++ js/popular.js
@@ -1,8 +1,18 @@
 let slides = document.getElementsByClassName('slider_pst'); 
-
 let visSlide = 0;
 let slidCount = slides.length;
 
+let contener = document.getElementById('dotsContainer'); 
+let dot;
+let dots = [];
+for(let i = 0; i < slidCount; i ++){
+    dot = document.createElement("input");
+    dot.className ='dots' + ' ' + i;
+    dot.type = "button";
+    contener.appendChild(dot);
+    dots[i] = dot;
+}
+dots[0].style.border = '4px solid white';
 let arrR = document.getElementById('arrPR');
 let arrL = document.getElementById('arrPL');
 console.log(slidCount);
@@ -36,9 +46,27 @@ function RSwipe(){
     timerSwipe = setInterval(RSwipe, timerDelay);
 }
 
+contener.addEventListener('click', dotsClick, false)
+
+function dotsClick(evt){
+    let num = Number(evt.target.className[5]);
+    console.log(num);
+    if(num >= 0 && num < slidCount)
+    {
+        //console.log(num);
+        clearTimeout(timerSwipe);
+        last = visSlide; 
+        visSlide = num;
+        chenge();
+        timerSwipe = setInterval(RSwipe, timerDelay);
+    }
+    
+}
 
 
 function chenge(){
+    dots[last].style.border = '0';
     slides[last].style.opacity ='0';
+    dots[visSlide].style.border = '5px solid white';
     slides[visSlide].style.opacity = '1';
 }
