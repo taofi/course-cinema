36afda52b3d82c6454f4a770831f8f2991b48678
taofi <taofi.stop.lego@gmail.com>
2023-04-20 15:19:58 +0300

    Signed-off-by: taofi <taofi.stop.lego@gmail.com>

--- js/afishaSlide.js
+++ js/afishaSlide.js
@@ -11,13 +11,43 @@ let shag = posterW;
 let countP = posters.length;
 
 let visiblePoster;
+let FunId;
+const delayF = 200;
 
-arrLA.addEventListener('click', function(){
+arrLA.addEventListener("mousedown", function() {
+    FunId = setInterval(arrLF, delayF);
+ });
+  
+ arrLA.addEventListener("mouseup", function() {
+    clearInterval(FunId);
+});
+arrLA.addEventListener("mouseleave", function() {
+    clearInterval(FunId);
+});
+
+arrLA.addEventListener('click', arrLF);
+
+function arrLF(){
     currentPos += shag;
     currentPos = Math.min(0, currentPos);
     move();
+}
+
+arrRA.addEventListener("mousedown", function() {
+    FunId = setInterval(arrRF, delayF);
+ });
+  
+ arrRA.addEventListener("mouseup", function() {
+    clearInterval(FunId);
 });
-arrRA.addEventListener('click', function(){
+arrRA.addEventListener("mouseleave", function() {
+    clearInterval(FunId);
+});
+
+
+arrRA.addEventListener('click', arrRF);
+
+function arrRF(){
     widthCont = document.getElementById('afishaContent').offsetWidth;
     visiblePoster = widthCont / posterW;
     let tmp = posterW*(Math.trunc(visiblePoster) - visiblePoster);
@@ -25,8 +55,19 @@ arrRA.addEventListener('click', function(){
     currentPos -= shag;
     currentPos = Math.max(currentPos, -(maxPos + 50))
     move();
-});
+}
 
 function move(){
     convAfish.style.transform = `translatex(${currentPos}px)`;
+}
+
+window.addEventListener("resize", changeWindow);
+
+function changeWindow() {
+    if (window.innerWidth <= 320) {
+        currentPos = 0;
+        move();
+    }else{
+        document.getElementById('afishaContent').transform = `translatex(0px)';
+    }
 }
